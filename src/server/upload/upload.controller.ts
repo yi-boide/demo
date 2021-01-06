2235702fe2b7b6ae64454e1fd188f60cf46e99a5
boide gui <454846659@qq.com>
2021-01-06 17:11:45 +0800

    描述

--- src/server/upload/upload.controller.ts
+++ src/server/upload/upload.controller.ts
@@ -4,7 +4,7 @@
  * @Autor: boide gui
  * @Date: 2020-12-31 13:15:53
  * @LastEditors: boide gui
- * @LastEditTime: 2021-01-06 14:02:06
+ * @LastEditTime: 2021-01-06 16:58:54
  */
 import {
   Controller,
@@ -13,14 +13,18 @@ import {
   UploadedFiles,
 } from '@nestjs/common';
 import { FilesInterceptor } from '@nestjs/platform-express';
+// swagger的展示配置
 import { ApiTags, ApiProperty, ApiConsumes, ApiBody } from '@nestjs/swagger';
 import { UploadService } from './upload.service';
 import { IHttpData } from '../../utils/relust';
+
 class FilesUploadDto {
+  // swagger配置项
   @ApiProperty({ type: 'array', items: { type: 'string', format: 'binary' } })
   file: any[];
 }
 
+// swagger该模块的标题
 @ApiTags('文件接口')
 @Controller('upload')
 export class UploadController {
@@ -31,22 +35,27 @@ export class UploadController {
   // @UseInterceptors(AnyFilesInterceptor())
   // AnyFilesInterceptor定义任意字段的名称
   @UseInterceptors(FilesInterceptor('file')) // file对应HTML表单的name属性
+  // swagger入参配置
   @ApiConsumes('multipart/form-data')
   @ApiBody({
     description: '选择文件',
     type: FilesUploadDto,
   })
   async UploadedFile(@UploadedFiles() files): Promise<IHttpData> {
+    // 接口严格要求以这个形式返回给前端
     const result: IHttpData = {
       code: 0,
       data: null,
       msg: '',
     };
+    // 若传入的文件为空，直接返回
     if (!files || files.length === 0) {
       result.code = -1;
       result.msg = '未选择文件';
       return result;
     }
+    // 调用service的存储文件方法，传入前端传来的文件数组
+    // 成功后会将文件信息返回给前端
     const data = await this.uploadService.create(files);
     result.code = 0;
     result.data = data;
