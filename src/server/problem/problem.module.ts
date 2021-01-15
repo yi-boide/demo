30adef0e9ca0e1c480b9f9aef9694d17ead17a40
boide gui <454846659@qq.com>
2021-01-15 17:31:27 +0800

    module

--- src/server/problem/problem.module.ts
+++ src/server/problem/problem.module.ts
@@ -1,9 +1,20 @@
+/*
+ * @Descriptin:
+ * @Version: 0.1
+ * @Autor: boide gui
+ * @Date: 2021-01-15 17:21:06
+ * @LastEditors: boide gui
+ * @LastEditTime: 2021-01-15 17:31:15
+ */
 import { Module } from '@nestjs/common';
 import { ProblemService } from './problem.service';
 import { ProblemController } from './problem.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Problem } from './problem.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Problem])],
   providers: [ProblemService],
-  controllers: [ProblemController]
+  controllers: [ProblemController],
 })
 export class ProblemModule {}
