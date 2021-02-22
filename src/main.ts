522f3c902efbf5980f005925b6db3a5855c54fd4
boide gui <454846659@qq.com>
2021-02-22 19:48:22 +0800

    1

--- src/main.ts
+++ src/main.ts
@@ -22,8 +22,8 @@ async function bootstrap() {
   // '/public' 是路由名称，即你访问的路径为：host/public
   // serveStatic 为 serve-static 导入的中间件，其中'../public' 为本项目相对于src目录的绝对地址
   app.use(
-    '/public',
-    serveStatic(join(__dirname, '../public'), {
+    '/static',
+    serveStatic(join(__dirname, '../static'), {
       maxAge: '1d',
       extensions: ['jpg', 'jpeg', 'png', 'gif', 'mp4'],
     }),
